edad8a19d397233adb019c812d2a5b48e082b4a1
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-09-24 11:51:02 +0530

    Added 04_Header-file-and-operators.cpp

---
+++ 04_Header-file-and-operators.cpp
@@ -0,0 +1,45 @@
+// There are two types of header files:
+// 1. System header files: It comes with the compiler
+#include<iostream>
+// 2. User defined header files: It is written by the programmer
+// #include "this.h" //--> This will produce an error if this.h is not present in the current directory
+using namespace std;
+int main() {
+    int a = 3, b = 8;
+    cout<<"Operators in C++:"<<endl;
+    cout<<"Following are the operators in C++:"<<endl;
+
+    // Arithmetic Operators
+    cout<<"\nArithmetic Operators:\n";
+    cout<<"a = "<<a<<", b = "<<b<<endl;
+    cout<<"a + b = "<<a+b<<endl;
+    cout<<"a - b = "<<a-b<<endl;
+    cout<<"a * b = "<<a*b<<endl;
+    cout<<"a / b = "<<a/b<<endl;
+    cout<<"a % b = "<<a%b<<endl;
+    cout<<"a++ = "<<a<<endl;  // Post increment (assignment then increment)
+    cout<<"++b = "<<b<<endl;  // Pre increment  (increment then assignment)
+
+    // Assignment Operators --> used to assign values to variables
+    // int a = 2, b = 89;
+    // int ch = 'a'; 
+
+    // Comparison Operators
+    cout<<"\nComparison Operators:\n";
+    cout<<"a = "<<a<<", b = "<<b<<endl;
+    cout<<"a == b = "<<(a==b)<<endl;  // we have to put a bracket otherwise compiler will be confused which operator to evaluate first
+    cout<<"a != b = "<<(a!=b)<<endl;
+    cout<<"a <= b = "<<(a<=b)<<endl;
+    cout<<"a >= b = "<<(a>=b)<<endl;
+    cout<<"a < b = "<<(a<b)<<endl;
+    cout<<"a > b = "<<(a>b)<<endl;
+
+    // Logical Operators
+    cout<<"\nLogical Operators : ";
+    cout<<"a = "<<a<<", b = "<<b<<endl;
+    cout<<"((a==b) && (a<b)) = "<<((a==b) && (a<b))<<endl;  // we have to put a bracket otherwise compiler will be confused which operator to evaluate first
+    cout<<"((a>b) || (a<b)) = "<<((a>b) || (a<b))<<endl;
+    cout<<"!((a>b) || (a<b)) = "<<!((a>b) || (a<b))<<endl;
+
+    return 0;
+}
