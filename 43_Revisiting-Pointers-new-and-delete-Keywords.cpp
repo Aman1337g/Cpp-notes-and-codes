4181d6acde302db7d7fc44308d2d75761e670137
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-10-17 17:11:18 +0530

    Fomatted

--- 43_Revisiting-Pointers-new-and-delete-Keywords.cpp
+++ 43_Revisiting-Pointers-new-and-delete-Keywords.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     // Basic Example
     int a = 55;
-    int* ptr = &a;
+    int *ptr = &a;
     *ptr = 30;
-    cout<<"The value of a is "<<*(ptr)<<endl;
+    cout << "The value of a is " << *(ptr) << endl;
 
     // new Keyword  //--> dynamically allocating memory
     // int* p = new int(40);
-    float* p = new float(32.98);
-    cout<<"\nThe value at address p("<<p<<") is "<<*(p)<<endl;
+    float *p = new float(32.98);
+    cout << "\nThe value at address p(" << p << ") is " << *(p) << endl;
 
     int *arr = new int[5];
     arr[0] = 10;
@@ -19,17 +20,18 @@ int main() {
     arr[2] = 30;
     arr[3] = 40;
     arr[4] = 50;
-    cout<<"\nThe value of arr[0] is "<<arr[0]<<endl;
-    delete[] arr;   //--> frees the array memory
-    cout<<endl;
-    for(int i=0; i<5; i++) {
-        cout<<"The value of arr["<<i<<"] is "<<*(arr+i)<<endl;
+    cout << "\nThe value of arr[0] is " << arr[0] << endl;
+    delete[] arr; //--> frees the array memory
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "The value of arr[" << i << "] is " << *(arr + i) << endl;
     }
 
-    /* delete Operator -> Delete is an operator that is used to destroy array and non-array(pointer) objects 
+    /* delete Operator -> Delete is an operator that is used to destroy array and non-array(pointer) objects
     which are created by new expression.*/
     delete p;
-    cout<<"\nThe value at address p("<<p<<") is "<<*(p)<<endl;  // --> now memory at address "p" is free
+    cout << "\nThe value at address p(" << p << ") is " << *(p) << endl; // --> now memory at address "p" is free
     return 0;
 }
 /*
