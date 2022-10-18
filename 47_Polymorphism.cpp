c04fbc3716239509178ba0ab4e482563b5d3988b
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-10-18 18:55:20 +0530

    Modified

---
+++ 47_Polymorphism.cpp
@@ -0,0 +1,13 @@
+/*
+    Polymorphism
+    - one name and multiple forms
+    - eg. Function overloading, operator overloading
+    - eg. Virtual Functions
+
+    Two types :
+    1. Compile time (static or early binding) polymorphism. Can be acheived by -
+        - Function Overloading
+        - Operator Overloading
+    2. Run time (late binding) polymorphism. Can be acheived by -
+        - Virtual Functions
+*/
