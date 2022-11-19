2c629da22b0ed810638bdc4b261eef3ae2e4bfba
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-11-20 00:05:22 +0530

    Added corrected output

--- 66_Header-Files/swap.cpp
+++ 66_Header-Files/swap.cpp
@@ -3,18 +3,19 @@
 using namespace std;
 int main()
 {
-    int x, y;
-    cout << "Enter x and y : ";
-    cin >> x >> y;
-    cout << "Before Swapping\n"
-         << "x = " << x << ", y = " << y << endl;
-    swap(x, y);
-    cout << "After Swapping\n"
-         << "x = " << x << ", y = " << y << endl;
+     int x, y;
+     cout << "Enter x and y : ";
+     cin >> x >> y;
+     cout << "Before Swapping\n"
+          << "x = " << x << ", y = " << y << endl;
+     swap(x, y);
+     cout << "After Swapping\n"
+          << "x = " << x << ", y = " << y << endl;
 }
 /*
-OUPUT
+OUTPUT
 
+Enter x and y : 45 5
 Before Swapping
 x = 45, y = 5
 After Swapping
