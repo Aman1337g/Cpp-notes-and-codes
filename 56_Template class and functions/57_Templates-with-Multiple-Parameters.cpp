42e6ef558b0aabb9332790f37341bd6847ab9c2f
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-12-13 07:39:57 +0530

    Created counter and corrected spelling

--- 56_Template class and functions/57_Templates-with-Multiple-Parameters.cpp
+++ 56_Template class and functions/57_Templates-with-Multiple-Parameters.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 /*
-CLASS TEMPLATE WIHT MULTIPLE PARAMETERS (ONE, TOW OR MORE THAN TWO)
+CLASS TEMPLATE WITH MULTIPLE PARAMETERS (ONE, TOW OR MORE THAN TWO)
 template <class T1, class T2, ......(COMMA SEPERATED)>
 class nameOfClass {
     // body
 }
 */
+static int count; // Created counter for objects
 
 template <class T1, class T2>
 class myClass
@@ -17,13 +18,15 @@ public:
     T2 data2;
     myClass(T1 a, T2 b)
     {
+        count++;
+        cout << "Object " << count << " created!!" << endl;
         data1 = a;
         data2 = b;
     }
     void display()
     {
         cout << this->data1 << endl
-             << this->data2;
+             << this->data2 << endl;
     }
 };
 
@@ -31,12 +34,19 @@ int main()
 {
     myClass<int, char> obj(7, 'a');
     obj.display();
+    myClass<string, double> obj2("Aman", 389.34);
+    obj2.display();
 
     return 0;
 }
 /*
 OUTPUT
 
+Object 1 created!!
 7
 a
+Object 2 created!!
+Aman
+389.34
+
 */
